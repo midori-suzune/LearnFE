3e0ad3c567431cb83e6fa84c5ca42207da0608c0
midori-suzune <chuhieu457@gmail.com>
2026-05-11 22:53:16 +0700

    feat(tabs): implement tab functionality with optimized event handling
    * Added event listeners to tab buttons for dynamic content display.
    * Optimized tab click handling by converting NodeList to Array.
    * Updated styles for active tab indication.

--- Day19/problem2.js
+++ Day19/problem2.js
@@ -1,8 +1,34 @@
 const contentButton = document.querySelectorAll('.content-button button');
-contentButton.forEach(element => {
-    element.addEventListener('click', () => {
-        
-        element.classList.add('active');
-    });
+const contentText = document.querySelectorAll('.text-content');
+
+// way 1
+    // const handleTabClick = (event) => {
+    //     contentButton.forEach(item => {
+    //         item.classList.remove('active');
+    //     });
+    //     event.target.classList.add('active');
+    //     contentText.forEach(item => item.classList.remove('active'));
+    //     contentButton.forEach((item , key) => {        
+    //         if(item.classList.contains('active')){
+    //             contentText[key].classList.add('active');
+    //         }
+    //     });
+    // }
+
+    // contentButton.forEach(item => {
+    //     item.addEventListener('click', handleTabClick);
+    // });
+
+//  optimize 
+const handleTabClick2 = (event) => {
+    // Convert NodeList to Array and find the index of the clicked button
+    const index = Array.from(contentButton).indexOf(event.target);
+    contentButton.forEach(item => item.classList.remove('active'));
+    contentText.forEach(item => item.classList.remove('active'));
+    event.target.classList.add('active') ;
+    contentText[index].classList.add('active');
+}
+contentButton.forEach(item => {
+    item.addEventListener('click', handleTabClick2);
 });
 
