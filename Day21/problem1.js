33c9aa155d105515ee26631ef0def09c3dcda7d3
midori-suzune <chuhieu457@gmail.com>
2026-05-17 17:17:38 +0700

    feat(image-viewer): update image source and optimize click handling
    * Change image source to a higher resolution.
    * Simplify event handling for image selection.
    * Implement image navigation functionality with left/right icons.

--- Day21/problem1.js
+++ Day21/problem1.js
@@ -1,35 +1,51 @@
 const viewImage = document.querySelector(".view-image");
 const seeImage = document.querySelectorAll(".quick-see-image");
 const imageList = document.querySelectorAll(".image-container img");
-const arrayImage = Array.from(seeImage);
-console.log(arrayImage);
 let mainImage = document.querySelector(".image img");
-let index = 0;
-seeImage.forEach((item) => {
-    if (index === arrayImage.length) {
-        index = 0;
-    } else {
-        item.addEventListener("click", (event) => {
-            console.log(event.currentTarget);
-            console.log(arrayImage.indexOf(event.target.classList));
-            mainImage.src = imageList[index].src;
-            viewImage.classList.add("active");
-            index++;
-        });
-    }
+seeImage.forEach((item, key) => {
+    item.addEventListener("click", (event) => {
+        mainImage.src = imageList[key].src;
+        viewImage.classList.add("active");
+    });
 });
+
+const iconLeft = document.querySelector(".icon-left");
+const iconRight = document.querySelector(".icon-right");
+
+const changeImage = () => {
+    let index = 0;
+    iconLeft.addEventListener('click', () => {
+        let currentImage = mainImage;
+        imageList.forEach((e, key) => {
+            if (e.src === currentImage.src) {
+                index = key;
+            }
+        });
+        if (index > 0) {
+            mainImage.src = imageList[index - 1].src;
+        }
+        index--;
+    });
+    iconRight.addEventListener('click', (right) => {
+        let currentImage = mainImage;
+        imageList.forEach((e, key) => {
+            if (e.src === currentImage.src) {
+                index = key;
+            }
+        });
+        if (index < ( imageList.length - 1 )  ) {
+            mainImage.src = imageList[index + 1].src;
+        }
+        index++;
+    });
+};
+
+changeImage();
+
 viewImage.addEventListener("click", (event) => {
     if (event.target.classList.contains("view-image")) {
         event.target.classList.remove("active");
     }
 });
 
-const iconLeft = document.querySelector(".icon-left");
-const iconRight = document.querySelector(".icon-right");
 
-// console.log(arrayImage[0].currentSrc);
-// const changeImage = () => {
-//     iconLeft.addEventListener('click', () => {
-
-//     });
-// }
